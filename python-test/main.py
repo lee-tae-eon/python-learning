d6637bc07526df5cd2eb30ed636b2c5c5e083448
lee-tae-eon <mktears0609@gmail.com>
2022-09-07 08:34:19 +0900

    ADD: selenium

--- python-test/main.py
+++ python-test/main.py
@@ -1,3 +1,21 @@
-from extractors.www import extract_wwr_jobs
+from bs4 import BeautifulSoup
+from requests import get
 
-jobs = extract_wwr_jobs("react")
+
+base_url = "https://kr.indeed.com/jobs?q="
+search_terms = "react"
+
+
+response = get(f"{base_url}{search_terms}")
+
+
+if response.status_code != 200:
+  print("can't request page")
+  print(response._content)
+else:
+  soup = BeautifulSoup(response.text, "html.parser")
+
+  job_list = soup.find("ul", class_="jobsearch-ResultsList")
+  jobs = job_list.find_all("li", recursive=False)
+  print(len(jobs))
+
