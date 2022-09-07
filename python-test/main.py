3868668b84b0179e0f0b4e77ec450a0535d38a04
lee-tae-eon <mktears0609@gmail.com>
2022-09-07 10:17:43 +0900

    ADD: webdriver && selenium

--- python-test/main.py
+++ python-test/main.py
@@ -1,21 +1,35 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from requests import get
 
+
+options = Options()
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
+
+
+
+browser = webdriver.Chrome(options=options)
 
 base_url = "https://kr.indeed.com/jobs?q="
 search_terms = "react"
 
+sleep(1)
 
-response = get(f"{base_url}{search_terms}")
+browser.get(f"{base_url}{search_terms}")
 
+soup = BeautifulSoup(browser.page_source, "html.parser")
 
-if response.status_code != 200:
-  print("can't request page")
-  print(response._content)
-else:
-  soup = BeautifulSoup(response.text, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all("li", recursive=False)
+for job in jobs:
+  zone = job.find("div", class_="mosaic-zone")
+  if zone == None:
+    print("job li")
+  else:
+    print("mosaic li")
+  print("===========================================")
 
-  job_list = soup.find("ul", class_="jobsearch-ResultsList")
-  jobs = job_list.find_all("li", recursive=False)
-  print(len(jobs))
+browser.close()
 
