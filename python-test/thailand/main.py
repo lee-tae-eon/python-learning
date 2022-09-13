5bd5f93bb1fbffc421ebab8551e2aceb521da1c0
lee-tae-eon <mktears0609@gmail.com>
2022-09-13 12:48:38 +0900

    TEST: test

--- python-test/thailand/main.py
+++ python-test/thailand/main.py
@@ -26,6 +26,7 @@ def get_final_hs_code_detil_english(keyword):
   }
 
   response = requests.post(base_url, params=params)
+  sleep(1.5)
   if response.status_code != 200:
     print(f"status {response.status_code}")
   else:
@@ -39,7 +40,8 @@ def get_final_hs_code_detil_english(keyword):
       # ! tag가 없는 요소인 협정세율 네이밍 가져오기 br태그 기준
       _br = table.find_previous_sibling("br")
       # * 협정세율 네이밍
-      rate_title = table.previous_sibling if _br == None else _br.next_sibling
+      rate_title = table.previous_sibling.replace("\r\n","") if _br == None else _br.next_sibling.replace("\r\n","")
+      str_rate_title = rate_title.split(":")[0].strip() + ": " + rate_title.split(":")[1].strip()
 
       _tbody = table.find("tbody")
       _tr_list = _tbody.find_all("tr", reculsive = False)
@@ -47,26 +49,30 @@ def get_final_hs_code_detil_english(keyword):
       for tr in _tr_list:
         _td_list = tr.find_all("td")
 
-        hs_code = _td_list[1].string.strip()
+        sub_heading = _td_list[1].string.strip()
         description = _td_list[2].string.strip()
         # ad_valorem_rate = _td_list[3].select_one("font").get_text(strip=True) if len(_td_list)  == 8 else _td_list[3].text.strip()
         # unit = _td_list[3].select_one("font").get_text(strip=True) if len(_td_list)  == 8 else _td_list[4].text.strip()
         # baht = _td_list[3].select_one("font").get_text(strip=True) if len(_td_list)  == 8 else _td_list[5].text.strip()
-        ad_valorem_rate = "Exempted" if len(_td_list)  == 8 else _td_list[3].text.strip().replace("\r\n","").replace("**","")
+        ad_valorem_rate = "Exempted" if len(_td_list)  == 8 else _td_list[3].text.strip().replace("\r\n","").replace("**","").strip()
         unit = "Exempted" if len(_td_list)  == 8 else _td_list[4].text.strip().replace("\r\n","").replace("**","")
         baht = "Exempted" if len(_td_list)  == 8 else _td_list[5].text.strip().replace("\r\n","").replace("**","")
         start_date = _td_list[-3].string.strip()
         end_date = _td_list[-2].string.strip()
+
         rate_dict = {
-          "rate_title": rate_title.strip(),
-          "hs_code" : hs_code,
-          "description" : description,
-          "ad_valorem_rate" : ad_valorem_rate,
-          "unit" : unit,
-          "baht" : baht,
-          "start_date" : start_date,
-          "end_date" : end_date,
+          f"{str_rate_title}": {
+            "sub_heading" : sub_heading,
+            "description" : description,
+            "ad_valorem_rate" : ad_valorem_rate,
+            "unit" : unit,
+            "baht" : baht,
+            "start_date" : start_date,
+            "end_date" : end_date,
+          }
         }
+        print(rate_dict)
+
         list_custom_detail.append(rate_dict)
 
 
@@ -90,11 +96,11 @@ def get_hs_code():
 
   # for code in search_list:
   #   params = {"lang": "t", "taffCode": f"{code}", "docBegnDate": "07/09/2565", "param": "search"}
-  params = {"lang": "t", "taffCode": "02", "docBegnDate": "07/09/2565", "param": "search"}
+  params = {"lang": "t", "taffCode": "0813", "docBegnDate": "07/09/2565", "param": "search"}
 
-  sleep(2)
 
   response = requests.post(base_url, params=params)
+  sleep(1.5)
 
   if response.status_code != 200:
     print(f"status {response.status_code}")
@@ -105,15 +111,13 @@ def get_hs_code():
     tr_list = t_body.find_all("tr")
 
     for code in tr_list:
-      sleep(1)
+
       hs_code = code.find("a").string.strip() if code.find("a") != None else " "
       custom_rate_dict = [{}]
 
       if len(hs_code.replace(".","")) >= 8:
         custom_rate_dict = get_final_hs_code_detil_english(hs_code.replace(".",""))
-        if(hs_code.replace(".","") == "02031200"):
-          print(hs_code, "hs-code")
-          print(custom_rate_dict)
+
       t_desc = code.select_one("td:nth-last-child(2)").get_text(strip=True).replace(",", " ")
       e_desc = code.select_one("td:last-child").get_text(strip=True).replace(",", " ")
 
@@ -130,23 +134,17 @@ def get_hs_code():
       code_dict = {"hs_code": hs_code,"indent": indent, "origin": t_desc, "english": e_desc, "ceiling_rate": "",  "general_Rate": "", "custom_rate": custom_rate_dict }
       results.append(code_dict)
     print(results)
-    # file = open(f"thiland.csv", "w")
+    file = open(f"thiland.csv", "w")
 
-    # file.write("hscode, indent, origin, english\n")
+    file.write("hscode, indent, origin, english, custom_rate\n")
 
-    # for result in results:
-    #   file.write(f"{result['hs_code']},{result['indent']},{result['origin']},{result['english']}\n")
+    for result in results:
+      file.write(f"{result['hs_code']},{result['indent']},{result['origin']},{result['english']},{result['custom_rate']}\n")
 
-    # file.close()
+    file.close()
 
 
 get_hs_code()
-
-
-
-
-
-
 
 
 
